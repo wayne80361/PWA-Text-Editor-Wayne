265b00a508de0d8bf6316e7858f7ab397f146e10
wayne80361 <wayne80361@gmail.com>
2023-10-29 02:59:26 -0400

    set WebpackPwaManifest plugins in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,11 +23,28 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
-      new MiniCssExtractPlugin(),
-
+      // new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: "./src/sw.js",
-        swDest: "service-worker.js",
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Takes notes with JavaScript syntax highlighting!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join("assets"),
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+        start_url: "./",
       }),
     ],
 
@@ -35,7 +52,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
