b978e954113ffca44ad7f16651d0e38640ee7027
wayne80361 <wayne80361@gmail.com>
2023-10-29 23:52:40 -0400

    webpack.config, remove MiniCssExtractPlugIn, change swDest from src-sw.js to service-worker.js, load babel plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,73 +2,74 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "Webpack Plugin",
-      }),
-      // new MiniCssExtractPlugin(),
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
-      new WebpackPwaManifest({
-        name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "Takes notes with JavaScript syntax highlighting!",
-        background_color: "#ffffff",
-        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
-        publicPath: "./",
-        icons: [
-          {
-            src: path.resolve("./src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-            destination: path.join("assets"),
-          },
-        ],
-        orientation: "portrait",
-        display: "standalone",
-        start_url: "./",
-      }),
-    ],
-
-    module: {
-      rules: [
+// module.exports = () => {
+module.exports = {
+  // return {
+  mode: "development",
+  entry: {
+    main: "./src/js/index.js",
+    install: "./src/js/install.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "JATE",
+    }),
+    new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "service-worker.js",
+    }),
+    new WebpackPwaManifest({
+      name: "Just Another Text Editor",
+      short_name: "J.A.T.E",
+      description: "Takes notes with JavaScript syntax highlighting!",
+      background_color: "#0e0f0c",
+      theme_color: "#0e0f0c",
+      crossorigin: "use-credentials",
+      publicPath: "./",
+      icons: [
         {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
+          src: path.resolve("./src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets"),
         },
       ],
-    },
-  };
+      orientation: "portrait",
+      display: "standalone",
+      start_url: "./",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/transform-runtime",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
+// };
