01775c4234b9cea09938825f96653b4a6b10659b
wayne80361 <wayne80361@gmail.com>
2023-10-30 00:21:41 -0400

    modify install.js so the install button will display/remove based on installaztion states

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,11 +3,13 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+
   // Store the triggered events
   window.deferredPrompt = event;
 
-  // Remove the hidden class from the button.
-  butInstall.classList.toggle("hidden", false);
+  // Show the install button.
+  butInstall.style.display = "inline-block";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -24,11 +26,15 @@ butInstall.addEventListener("click", async () => {
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
 
-  butInstall.classList.toggle("hidden", true);
+  // remove install button
+  butInstall.style.display = "none";
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
   // Clear prompt
   window.deferredPrompt = null;
+
+  // remove install button
+  butInstall.style.display = "none";
 });
