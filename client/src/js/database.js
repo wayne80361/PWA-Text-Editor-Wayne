6508b0a52355ed4b9cf0e58207f02856c0cf3518
wayne80361 <wayne80361@gmail.com>
2023-10-30 00:00:33 -0400

    modify the putDb and getDb so it can read and write properly

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,35 +16,35 @@ const initdb = async () =>
 
 // export const putDb = async (content) => console.error('putDb not implemented');
 export const putDb = async (content) => {
+  console.error("putDb not implemented");
   console.log("PUT to the database");
   const db = await openDB("jate", 1);
   const tx = db.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  // Use the `put` method to add or update a record in the object store
-  const request = store.put({ content });
-  // await store.put({ content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
 
-  // await tx.done;
   console.log("Data added to the database.", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error("getDb not implemented");
 export const getDb = async () => {
+  console.error("getDb not implemented");
   console.log("GET all from the database");
   const db = await openDB("jate", 1);
-  const store = db.transaction("jate").objectStore("jate");
-  // const tx = db.transaction("jate", "readonly");
-  // const store = tx.objectStore("jate");
-  // Use the `getAll` method to retrieve all records from the object store
-  const allData = await store.getAll();
-  // const request = store.getAll();
-  // const result = await request;
+  // const store = db.transaction("jate").objectStore("jate");
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
 
-  return allData.map((item) => item.content);
-  // console.log("result.value", result);
-  // return result;
+  // Use the `getAll` method to retrieve all records from the object store
+  // const allData = await store.getAll();
+  const request = store.getAll();
+  const result = await request;
+
+  // return allData.map((item) => item.content);
+  console.log("result.value", result);
+  return result.value;
 };
 
 initdb();
